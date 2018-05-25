6fc2fe80b42011a94eef20520a68fa1f6e81ce4e
Carmen Shing <carmenshing@gmail.com>
2018-05-25 19:53:38 +0000

    Adds additional stretch work and kata practice

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,13 +1,18 @@
-var i = 100;
+for (var i = 100; i <= 200; i++) {
 
-for (i = 100; i <= 200; i++) {
   if (i % 4 === 0 && i % 3 === 0) {
     console.log ("LoopyLighthouse");
-  } else if (i % 4 === 0) {
+  }
+
+  else if (i % 4 === 0) {
     console.log ("Lighthouse");
-  } else if (i % 3 === 0) {
+  }
+
+  else if (i % 3 === 0) {
     console.log ("Loopy");
-  } else {
+  }
+
+  else {
     console.log(i);
   }
 }
